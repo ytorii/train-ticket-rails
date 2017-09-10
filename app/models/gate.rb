092fb6d7ef8dee09bd513b61f6275a36009944d8
ytorii <metalwarrior_type_jp@hotmail.com>
2017-09-10 16:42:02 +0900

    Answer for Ex3

--- app/models/gate.rb
+++ app/models/gate.rb
@@ -1,6 +1,6 @@
 # NOTE: Gate = 改札機のイメージ
 class Gate < ApplicationRecord
-  FARES = [150, 190].freeze
+  FARES = [150, 190, Float::INFINITY].freeze
 
   validates :name, presence: true, uniqueness: true
   validates :station_number, presence: true, uniqueness: true
@@ -8,6 +8,7 @@ class Gate < ApplicationRecord
   scope :order_by_station_number, -> { order(:station_number) }
 
   def exit?(ticket)
+    #return false if self.id == ticket.entered_gate_id
     fare_pos =
       (ticket.entered_gate.station_number - self.station_number).abs - 1
     ticket.fare >= FARES[fare_pos]
