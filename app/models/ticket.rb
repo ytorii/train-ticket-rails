fa3ff6d80ed24e1eaa062f978bc26f87e5308f43
ytorii <metalwarrior_type_jp@hotmail.com>
2017-09-10 19:10:47 +0900

    Answer for Ex4

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,13 @@ class Ticket < ApplicationRecord
   belongs_to :exited_gate, class_name: 'Gate', foreign_key: 'exited_gate_id', required: false
   validates :fare, presence: true, inclusion: Gate::FARES
   validates :entered_gate_id, presence: true
+
+  before_update :sufficent_fare?
+
+  def sufficent_fare?
+    unless exited_gate.exit?(self)
+      errors[:base] << '降車駅 では降車できません。'
+      throw(:abort) 
+    end
+  end
 end
