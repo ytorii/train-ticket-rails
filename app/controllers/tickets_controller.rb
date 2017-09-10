2fa82bcf0812f9475a6cc8379030bc1d7c0906d7
ytorii <metalwarrior_type_jp@hotmail.com>
2017-09-10 19:15:20 +0900

    Answer for Ex5

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,6 @@
 class TicketsController < ApplicationController
   before_action :load_ticket, only: %i(edit update show)
+  before_action :used_ticket?, only: %i(edit update)
 
   def index
     redirect_to root_path
@@ -45,5 +46,11 @@ class TicketsController < ApplicationController
 
   def load_ticket
     @ticket = Ticket.find(params[:id])
+  end
+
+  def used_ticket?
+    if @ticket.entered_gate_id && @ticket.exited_gate_id
+      redirect_to root_path, alert: '降車済みの切符です。'
+    end
   end
 end
