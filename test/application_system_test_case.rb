7ed9cad388b9f2c7a17c5237d39f09b1aa3652de
ytorii <metalwarrior_type_jp@hotmail.com>
2017-09-10 16:40:44 +0900

    Add chrome container to peform system test case in docker

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,15 @@
 require "test_helper"
+require "socket"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400],
+    options: { url:"http://chrome:4444/wd/hub" }
+
+  def setup
+    host! "http://#{IPSocket.getaddress(Socket.gethostname)}"
+
+    super
+  end
+  #caps = Selenium::WebDriver::Remote::Capabilities.chrome( "chromeOptions" => { "binary" => '/usr/bin/chromium-browser', "args" => %w(headless no-sandbox disable-gpu) })
+  #driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { desired_capabilities: caps, driver_path: '/usr/bin/chromedriver' }
 end
